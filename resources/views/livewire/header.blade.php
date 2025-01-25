a4d9ebb16fceb629115c5d0ab66c3168bd599298
yogesh1663 <yogesh.code23@gmail.com>
2025-01-25 10:52:33 +0545

    feat: create livewire layout and Header component and integrate bootstrap

---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,9 @@
+<!-- As a heading -->
+<nav class="navbar bg-primary">
+    <div class="container-fluid">
+        <div class="container d-flex justify-content-between align-items-center" style="max-width: 900px">
+            <span class="mb-0 text-white navbar-brand h1">Livewire CRUD</span>
+            <a href="" class="btn btn-sm btn-success">Create Post</a>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts',Post::class)->name('posts.index');
