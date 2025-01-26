633a29619c8f8ee42bd1e870589f05087a2d365a
yogesh1663 <yogesh.code23@gmail.com>
2025-01-26 11:30:35 +0545

    feat(posts): add pagination to posts index page

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,16 +4,17 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 class PostList extends Component
 {
-    public $posts;
-    public function mount(){
-        $this->posts = Post::all();
-    }
-
+    use WithPagination, WithoutUrlPagination;
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
-        return view('livewire.post-list');
+        return view('livewire.post-list',[
+            'posts' => Post::paginate(5),
+        ]);
     }
 }
